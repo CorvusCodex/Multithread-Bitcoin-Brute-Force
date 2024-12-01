187a9b3ecce99c7d9dcaf90aeee3751f792bfe5c
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2024-12-01 09:34:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ process.title = "Multithread Bitcoin Brute Force by Corvus Codex";
 //BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3
 //ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
 //Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
+//Ko-fi: https://ko-fi.com/s/e059759b3b
 
 const readline = require('readline');
 const { spawn } = require('child_process');
@@ -25,7 +26,7 @@ function credit(){
   console.log("BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3");
   console.log("ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0");
   console.log("Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex");
-  console.log("Buy standalone Windows app: https://www.buymeacoffee.com/CorvusCodex/e/142988");
+  console.log("Buy standalone Windows app: https://ko-fi.com/s/e059759b3b");
   console.log("=================================================================");
 
 };
